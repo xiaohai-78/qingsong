c037fc76facc10c7801feeeae9247f6cc1918cc1
xiaohai78 <yuntao@xbongbong.com>
2024-07-15 22:13:51 +0800

    init: 初始化基本环境信息-漏掉的配置信息

---
+++ qingsong-gateway/src/main/java/com/xiaohai/qingsonggateway/QingsongGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohai.qingsonggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingsongGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingsongGatewayApplication.class, args);
+    }
+
+}
